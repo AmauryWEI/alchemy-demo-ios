2e85411273f7b7ead85c5d01486bc5819710a773
Amaury WEI <amaury.wei@gmail.com>
2022-12-05 18:06:43 +0100

    Create initial XCode iOS 15.0 project

---
+++ alchemy-demo-ios/alchemy-demo-ios/alchemy_demo_iosApp.swift
@@ -0,0 +1,17 @@
+//
+//  alchemy_demo_iosApp.swift
+//  alchemy-demo-ios
+//
+//  Created by Amaury WEI on 05.12.22.
+//
+
+import SwiftUI
+
+@main
+struct alchemy_demo_iosApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
